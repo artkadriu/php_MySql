8fc0f453cf499194be67384c7cbcb470e7ef3630
Student <artkadriu80@gmail.com~>
2024-10-16 17:47:45 +0200

    Create index.php

---
+++ module_3/index.php
@@ -0,0 +1,53 @@
+<?php 
+
+$num = -1;
+
+if($num<0){
+   // echo "$num is less than 0";
+}
+
+$age = 12;
+
+if(($age>12)  && ($age < 15)); {
+   // echo 'WE salee for youu';
+}
+
+if($age> 18){
+   // echo "You are an adult";
+}
+else{
+ //   echo "Your are under 18";
+}
+
+if ($num>0){
+   // echo "The value of\$num is a negative number";
+}elseif ($num==0){
+    //echo "The value of \$num is equal to zero";
+
+}else {
+    //echo "The value is \$num is a positive number";
+}
+
+// Write a program that takes a student's score as input and outputs their grades with this system: A: 90-100, B: 80-89, C:70-79, D: 60-69, F: below 60
+
+$score = 100;
+
+if (($score >= 90)  && ($score <= 100));
+{
+    echo "you get an A";
+ }elseif  (($score >=80 )  && ($score <= 89));
+ {
+     echo "you get an B";
+ } 
+ elseif
+ (($score >=70)  && ($score <= 79));
+{
+    echo "you get an C";
+}
+
+
+
+
+
+
+?>
