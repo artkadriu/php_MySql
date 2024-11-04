3fc838f1ea40c89bc39acfbb0356d83aff715a6e
Artkadriuu <artkadriu80@gmail.com>
2024-11-04 17:43:57 +0100

    Update index.php
    dfadfasdasda

--- lesson6/index.php
+++ lesson6/index.php
@@ -5,6 +5,22 @@ $dogs = array (
     array('Husky','Siberia',15),
     array('bulldog','England',10),
 );
-echo $dogs[0][0]. "Origin:" . $dogs[2] [2].".<br>".  "lifespan:". $dogs[1] [2];
+echo $dogs[0][0]. "Origin:" .$dogs[0] [1].""  ",life span:". $dogs[0] [2]. "<br>";
+echo $dogs[1][0]. "Origin:" .$dogs[1] [1].""  ",life span:". $dogs[1] [2]. "<br>";
+echo $dogs[2][0]. "Origin:" .$dogs[2] [1].""  ",life span:". $dogs[2] [2]. "<br>";
+
+for ($row = 0; $row<3; $row++){
+    echo "<p><b>Row number $row </b></p>";
+    echo "<ul>";
+    for ($col = 0; $col<3; $col++){
+        echo "<li>". $
+    }
+}
+
+
+
+
+
+
 
 ?>
