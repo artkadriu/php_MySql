593bd605cf71ca5a1fb39d9ece0f7859ba9a9d02
artkadriuu <artkadriu80@gmail.com>
2024-11-06 17:04:13 +0100

    Update nested_loop.php
    adasdasdasdasd

--- lesson6/nested_loop.php
+++ lesson6/nested_loop.php
@@ -1,10 +1,10 @@
 <?php 
-$arrays = array (
-    array(1,2,3);
-    array(1,2,3);
-    array(1,2,3);
+//$arrays = array (
+   // array(1,2,3);
+   // array(1,2,3);
+   // array(1,2,3);
 
-);
+//);
 
 for ($i = 1; $i < 4; $i++){
     for($j= 1; $j < 4; $j++){
@@ -12,6 +12,33 @@ for ($i = 1; $i < 4; $i++){
     }
 }
 
+
+for ($i = 1; $i < 5; $i++){
+for($j= $i; $j <=5; $j++)
+    echo "&nbsp";
+}
+
+for ($j =1; $j <=$i; $j++){
+    echo " * ";
+}
+
+echo "<br/>";
+
+
+$grade = array(
+"Math" => "3",
+"Art" => "3",
+"History" => "3",
+"Music" => "3",
+
+);
+
+//echo "Math grade is ".$grade['Math'];
+
+foreach($grade as $subject => $grade){
+    echo "Subject:". $subject."/ Grade: ".$grade;
+ echo "<br>";
+}
 
 
 
