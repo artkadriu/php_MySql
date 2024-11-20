14887ad61434e80e3777dc3d246c3d5ed556b362
artkadriu <artkadriu80@gmail.com>
2024-11-20 17:43:24 +0100

    asdasdas
    sdasdasdas

---
+++ lesson9/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>our PHP form </title>
+</head>
+<body>
+ <form action="">
+    <label for="usermame">Username:</label>
+    <input type="text" id="username" name="username" placeholder="Username"><br><br>
+    <label for="password">Password: </label>
+    <input type="text" id="password" name="password" placeholder="Password"><br><br>
+    <input type="submit" value="Submit">
+ </form>
+</body>
+</html>
